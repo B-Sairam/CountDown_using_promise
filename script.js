a3484e7c096c54d0806dfa50d07d7384aa20de85
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2021-10-24 12:02:12 +0530

    callback

--- script.js
+++ script.js
@@ -1,80 +1,39 @@
-let heading = document.querySelector('h2');
+ let time = document.querySelector('.time');
+let countdown = 10;
+let pop = document.querySelector(".pop");
+let canvas = document.querySelector('#my-canvas')
 
-let para = new Promise(function (resolve) {
-    setTimeout(() => {
-        resolve('10')
-    }, 1000);
-})
-para.then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-       setTimeout(() => {
-           resolve('9')
-       }, 1000);
-    });
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('8')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('7')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('6')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('5')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('4')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('3')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('2')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('1')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-    return new Promise(function (resolve) {
-        setTimeout(() => {
-            resolve('Happy Independence Day')
-        }, 1000);
-    })
-}).then(function (data) {
-    heading.innerText=data;
-})
+setTimeout(()=>{
+    time.innerText = countdown--;
+    setTimeout(()=>{
+        time.innerText = countdown--;
+        setTimeout(()=>{
+            time.innerText = countdown--;
+            setTimeout(()=>{
+                time.innerText = countdown--;
+                setTimeout(()=>{
+                    time.innerText = countdown--;
+                    setTimeout(()=>{
+                        time.innerText = countdown--;
+                        setTimeout(()=>{
+                            time.innerText = countdown--;
+                            setTimeout(()=>{
+                                time.innerText = countdown--;
+                                setTimeout(()=>{
+                                    time.innerText = countdown--;
+                                    setTimeout(()=>{
+                                        time.innerText =countdown--;
+                                        setTimeout(()=>{
+                                            pop.classList.add('active') 
+                                            canvas.classList.add('active') 
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
